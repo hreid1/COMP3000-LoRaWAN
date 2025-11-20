6b6933491c3bf5633acfd361fbc064a92f58a2f4
Henry Reid <henryreid05@gmail.com>
2025-11-20 16:06:10 +0000

    work on machine learning model

--- sourcecode/my-app/src/ml/models/isolationforest.py
+++ sourcecode/my-app/src/ml/models/isolationforest.py
@@ -1,17 +1,18 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 
-n_samples, n_outliers = 120, 40
-rng = np.random.RandomState(0)
-covariance = np.array([[0.5, -0.1], [0.7, 0.4]])
-cluster_1 = 0.4 * rng.randn(n_samples, 2) @ covariance + np.array([2, 2])
-cluster_2 = 0.3 * rng.randn(n_samples, 2) + np.array([02, -2])
-outliers = rng.uniform(low=-4, high=4, size=(n_outliers, 2))
+df = pd.read_csv("../../datasets/normal_dataset.csv")
 
-X = np.concatenate([cluster_1, cluster_2, outliers])
-y = np.concatenate(
-    [np.ones((2 * n_samples), dtype=int), -np.ones((n_outliers), dtype=int)]
-)
+X = df.drop(columns="Time")
+y = df
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
+
+scatter = plt.scatter(X[:, 0], X[:, 1], c=y, s=20, edgecolor="k")
+handles, labels = scatter.legend_elements()
+plt.axis("square")
+plt.legend(handles = handles, labels=["outliers", "inliers"], title="true class")
+plt.show()
