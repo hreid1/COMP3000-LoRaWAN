47f3b487675139b32b5fc82720f9e1b75e5e0b43
Henry Reid <henryreid05@gmail.com>
2025-12-14 16:57:05 +0000

    working on topnavbar/sidenavbar issues@

--- sourcecode/my-app/src/components/Navbar/SideNavbar.jsx
+++ sourcecode/my-app/src/components/Navbar/SideNavbar.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import './SideNavbar.css'
+import Dots from '../../assets/dots.svg'
 
 const SideNavbar = () => {
   return (
     <div className='sideNavbar'>
-      <ul>
-        <li>Home</li>
-        <li>Devices</li>
-        <li>Logs</li>
-        <li>Maps</li>
-        <li>AI-Model</li>
-      </ul>
+      <img src={Dots} alt="" className='logo'/>
+      <div className='menuContent'>
+        <ul>
+          <li>Home</li>
+          <li>Devices</li>
+          <li>Logs</li>
+          <li>Maps</li>
+          <li>AI-Model</li>
+        </ul>
+      </div>
     </div>
   )
 }
