7a2b2ecabf0d644b43f80b1e7e9237011eb99f14
Henry Reid <henryreid05@gmail.com>
2026-01-18 16:04:07 +0000

    working on database

--- sourcecode/backend/lorawan/models.py
+++ sourcecode/backend/lorawan/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+# Packet
+    # Time: UTC
+    # NodeID: Integer
+    # MAC: string
+    # SF (Spreading Factor): Integer
+    # CF (Channel Frequency): Floating point
+    # TX (Transmission Power): Integer
+    # BW (Bandwidth): Integer
+    # CR (Coding Rate): Integer
+    # SNR (Singal to noise ratio): Floating point
+    # RSSI (Recieve Signal Strength Indicator): Floating Point
+    # PktSeqNum: Integer
+    # payload: Character
+    # payloadSize: Integer
+    # numReceivedPerNode[nodeNumber-1]: Integer
+    # PDRPerNode: Integer
+    # numReceivedPerNodePerWindow[nodeNumber]
+    # currentSeqNum: Integer
+    # lastSeqNumAtWindowStart[nodeNumber]: Integer
+    # pdrPerNodePerWindow[nodeNumber]: Integer
+    # interArrivalTime_s: Floating point
+    # interArrivalTimeMin: Floating point
+# Node / device
+
+
 # Create your models here.
+class Packet(models.Model):
+    time = models.DateTimeField("")
+
+class Node(models.Model):
+    nodeID = models.ForeignKey()
