21bafd51ba81ce54ae2a31c4e9875eee6667bc2e
Henry Reid <henryreid05@gmail.com>
2026-01-23 15:52:21 +0000

    added compose.yml which runs both docker containers

--- sourcecode/my-app/CICD.sh
+++ sourcecode/my-app/CICD.sh
@@ -6,8 +6,8 @@ VERSION=INSECURE
 docker rm -rf $APP_NAME 2>/dev/null || true
 docker build -t $APP_NAME:$VERSION .
 
-docker scout cves $APP_NAME:$VERSION --output ../../docs/security/vulns.report
+docker scout cves $APP_NAME:$VERSION --output ../docs/vulns.report
 docker scout cves $APP_NAME:$VERSION --only-severity critical --exit-code
-docker scout sbom --output ../../docs/security/$APP_NAME.sbom $APP_NAME:$VERSION
+docker scout sbom --output ../docs/$APP_NAME.sbom $APP_NAME:$VERSION
 
 docker run -d -p 5173:5173 --name myapp $APP_NAME:$VERSION
